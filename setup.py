e4ec251a3c2ac0be9f7f6bc35a482b28df6f161f
iii-org-tw <67881762+iii-org-tw@users.noreply.github.com>
2020-07-24 13:35:20 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="weighted_quantile",
+    version="0.0.1",
+    author="123",
+    author_email="123",
+    description="A function that returns 'world'",
+    url="https://github.com/iii-org-tw/Test_with_pip",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
